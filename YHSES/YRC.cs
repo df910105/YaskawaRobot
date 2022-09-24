428098d8de6cc7ae3224b20e4552c45526d2fc75
宇謙@Home <df910105@yahoo.com.tw>
2022-09-24 12:01:16 +0800

    add project.

---
+++ YHSES/YRC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+/**
+* @author $username$
+*
+* @date - $time$ 
+*/
+
+namespace YHSES
+{
+    public class YRC
+    {
+        UdpClient client = new UdpClient();
+    }
+}
