a8f6149368c2735c251a4f4532c6d0ac145c0ba4
宇謙@Home <df910105@yahoo.com.tw>
2022-09-24 22:31:29 +0800

    YRC1000Packet (ANS&REQ).

--- YHSES/YRC.cs
+++ YHSES/YRC.cs
@@ -6,10 +6,20 @@ using System.Net.Sockets;
 using System.Text;
 
 /**
-* YASKAWA High Speed Ethernet Server Functions for YRC1000
-*
-* @date - $time$ 
-*/
+ * 
+ * YASKAWA High Speed Ethernet Server Functions (C#)
+ * 
+ * This C# Library is implement from hsinkoyu/fs100 on GitHub (ref: https://github.com/hsinkoyu/fs100)
+ *
+ * Copyright (C) MIRDC 2022
+ * 
+ * Greatest Thanks To:
+ *    Hsinko Yu <hsinkoyu@fih-foxconn.com>
+ * 
+ * Authors:
+ *    Willy Wu <ycwu@mail.mirdc.org.tw>
+ *    
+ */
 
 namespace YHSES
 {
@@ -41,33 +51,31 @@ namespace YHSES
         public const byte HEADER_ACK_NOT_REQUEST = 1;
         public const uint HEADER_BLOCK_NUMBER_REQ = 0;
         public const string HEADER_RESERVED_2 = "99999999";
-        public const ushort HEADER_PADDING_USHORT = 0;
-        public const byte HEADER_PADDING_BYTE = 0;
+        public const ushort HEADER_PADDING_U16 = 0;
+        public const byte HEADER_PADDING_U8 = 0;
 
-        private readonly ushort data_size;
-        private readonly byte division;
-        private readonly byte ack;
-        private readonly byte req_id;
-        private readonly uint block_no;
+        public readonly ushort data_size;
+        public readonly byte division;
+        public readonly byte ack;
+        public readonly byte req_id;
+        public readonly uint block_no;
 
-        public YRC1000PacketHeader(ushort data_size, byte division, byte ack, byte req_id, uint block_no, byte[] packet = null)
+        public YRC1000PacketHeader(ushort _data_size, byte _division, byte _ack, byte _req_id, uint _block_no)
         {
-            if (packet != null)
-            {
-                this.data_size = BitConverter.ToUInt16(packet, 6);
-                this.division = packet[9];
-                this.ack = packet[10];
-                this.req_id = packet[11];
-                this.block_no = BitConverter.ToUInt32(packet, 12);
-            }
-            else
-            {
-                this.data_size = data_size;
-                this.division = division;
-                this.ack = ack;
-                this.req_id = req_id;
-                this.block_no = block_no;
-            }
+            data_size = _data_size;
+            division = _division;
+            ack = _ack;
+            req_id = _req_id;
+            block_no = _block_no;
+        }
+
+        public YRC1000PacketHeader(byte[] packet)
+        {
+            data_size = BitConverter.ToUInt16(packet, 6);
+            division = packet[9];
+            ack = packet[10];
+            req_id = packet[11];
+            block_no = BitConverter.ToUInt32(packet, 12);
         }
 
         public byte[] ToBytes()
@@ -81,6 +89,88 @@ namespace YHSES
             h.Append(req_id);
             h.Concat(BitConverter.GetBytes(block_no));
             h.Concat(Encoding.ASCII.GetBytes(HEADER_RESERVED_2));
+            return h.ToArray();
+        }
+    }
+
+    class YRC1000ReqPacket
+    {
+        readonly YRC1000PacketHeader header;
+        readonly ushort cmd_no;
+        readonly ushort inst;
+        readonly byte attr;
+        readonly byte service;
+        readonly byte[] data;
+
+        public YRC1000ReqPacket(byte division, byte req_id, ushort cmd_no, ushort inst, byte attr, byte service, byte[] data, ushort data_size)
+        {
+            header = new YRC1000PacketHeader(data_size, division,
+                YRC1000PacketHeader.HEADER_ACK_REQUEST, req_id, YRC1000PacketHeader.HEADER_BLOCK_NUMBER_REQ);
+            this.cmd_no = cmd_no;
+            this.inst = inst;
+            this.attr = attr;
+            this.service = service;
+            this.data = data;
+        }
+
+        public byte[] ToBytes()
+        {
+            IEnumerable<byte> h = header.ToBytes();
+            h.Concat(BitConverter.GetBytes(cmd_no));
+            h.Concat(BitConverter.GetBytes(inst));
+            h.Append(attr);
+            h.Append(service);
+            h.Concat(BitConverter.GetBytes(YRC1000PacketHeader.HEADER_PADDING_U16));
+            h.Concat(data);
+            return h.ToArray();
+        }
+
+        public YRC1000ReqPacket Clone(byte[] data = null)
+        {
+            if (data == null)
+            {
+                return new YRC1000ReqPacket(header.division, header.req_id, cmd_no, inst, attr, service, this.data, (ushort)this.data.Length);
+            }
+            else
+            {
+                return new YRC1000ReqPacket(header.division, header.req_id, cmd_no, inst, attr, service, data, (ushort)data.Length);
+            }
+        }
+    }
+
+    class YRC1000AnsPacket
+    {
+        readonly YRC1000PacketHeader header;
+        readonly byte service;
+        readonly byte status;
+        readonly byte added_status_size;
+        readonly ushort added_status;
+        readonly byte[] data;
+
+        public YRC1000AnsPacket(byte[] _packet)
+        {
+            header = new YRC1000PacketHeader(_packet);
+            service = _packet[24];
+            status = _packet[25];
+            added_status_size = _packet[26];
+            added_status = BitConverter.ToUInt16(_packet, 28);
+            data = _packet.Skip(YRC1000PacketHeader.HEADER_SIZE).Take(header.data_size).ToArray();
+        }
+
+        /// <summary>
+        /// For debug purpose.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToBytes()
+        {
+            IEnumerable<byte> h = header.ToBytes();
+            h.Append(service);
+            h.Append(status);
+            h.Append(added_status_size);
+            h.Append(YRC1000PacketHeader.HEADER_PADDING_U8);
+            h.Concat(BitConverter.GetBytes(added_status));
+            h.Concat(BitConverter.GetBytes(YRC1000PacketHeader.HEADER_PADDING_U16));
+            h.Concat(data);
             return h.ToArray();
         }
     }
