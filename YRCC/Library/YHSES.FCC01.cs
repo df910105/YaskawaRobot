be03f868a2cdeb7bf818c7841a5cdb993c793993
宇謙@Home <df910105@yahoo.com.tw>
2022-10-13 16:54:27 +0800

    add FCC01

---
+++ YRCC/Library/YHSES.FCC01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YRCC.Packet;
+
+namespace YRCC.Library
+{
+    partial class YHSES
+    {
+        public int FileDelete(string fileName, out ushort err_code)
+        {
+            try
+            {
+                byte[] bytes = utf_8.GetBytes(fileName);
+                var req = new PacketReq(PacketHeader.HEADER_DIVISION_FILE_CONTROL, 0,
+                    0x0, 0x0, 0x0, 0x09,
+                    bytes, (ushort)bytes.Length);
+                var ans = Transmit(req.ToBytes(), PORT_FILE_CONTROL);
+                err_code = ans.added_status;
+                return ans.status;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
